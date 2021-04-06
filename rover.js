1606df7c4bf807eb42d8a1f7440ca4ef185dd110
Dan Gower <>
2021-04-06 01:53:40 +0000

    got it back to where test 10 is written and fails, tests 1-9 all pass

--- rover.js
+++ rover.js
@@ -1,5 +1,24 @@
 class Rover {
-   // Write code here!
+   constructor (position, mode = "NORMAL", generatorWatts = 110){
+     this.position = position;
+     this.mode = mode;
+     this.generatorWatts = generatorWatts;
+   }
+
+  receiveMessage(message){
+    
+    let results = [];
+    
+    for (let i = 0; i< message.commands.length; i++){
+      results.push(message.commands[i])
+    };
+    
+    return {
+      message: message.name,
+      results: results 
+    }
+  }
+
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,34 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+it("constructor sets position and default values for mode and generatorWatts", function() {
+ let rover = new Rover(1234);
+  expect (rover.position).toEqual(1234);
+  expect (rover.mode).toEqual("NORMAL");
+  expect (rover.generatorWatts).toEqual(110);
+});
 
+it("response returned by receiveMessage contains name of message", function() {
+  let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+  let message = new Message('Another message!', commands);
+  let rover = new Rover(1234);
+  let roverMessage = rover.receiveMessage(message);
+  expect(roverMessage.message).toEqual(message.name)
+});
+
+it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+  let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+  let message = new Message('Another message!', commands);
+  let rover = new Rover(1234);
+  expect (rover.receiveMessage(message).results.length).toEqual(2)
+});
+
+it('responds correctly to status check command', function() {
+  let commands = new Command('STATUS_CHECK');
+  let message = new Message('Another message!', commands);
+  let rover = new Rover(1234);
+  let response = rover.receiveMessage(message);
+  expect (response.results).toEqual({completed: true, roverStatus: {mode: 'NORMAL', generatorWatts: 110, position: 1234}})
+})
+ 
 });
